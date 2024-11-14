303feb9c438a0def5a78c59acb3231a24b66f667
PrinceMuichkine <babacar.diop.bba@edhec.com>
2024-11-14 21:15:17 +0000

    go

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const withNextra = require("nextra")({
   themeConfig: "./src/theme.config.tsx",
 });
 
-module.exports = withNextra();
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Add custom webpack config here if needed
+    return config;
+  },
+  // Add any other Next.js config options here
+};
+
+module.exports = withNextra(nextConfig);
